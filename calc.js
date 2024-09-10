14bf82fd7b123c6916d7ce7cee012a9fb46c36c2
witchcraftsteel <racoonracer58@gmail.com>
2024-09-10 12:50:58 -0300

    Added "for" and "if-else"

--- calc.js
+++ calc.js
@@ -1,42 +1,34 @@
-var buttonPlus = document.getElementById('buttonPlus');
-var buttonMinus = document.getElementById('buttonMinus');
-var buttonMult = document.getElementById('buttonMult');
-var buttonDisc = document.getElementById('buttonDisc');
 
-function onButtonPlusClic() {
-    var inputnum1 = document.getElementById('inputnum1');
-    var inputnum2 = document.getElementById('inputnum2');
-    var numvalue1 = Number(inputnum1.value);
-    var numvalue2 = Number(inputnum2.value);
-    var summa = numvalue1 + numvalue2;
-    alert(summa);
-}
-function onButtonMinusClic() {
-    var inputnum1 = document.getElementById('inputnum1');
-    var inputnum2 = document.getElementById('inputnum2');
-    var numvalue1 = Number(inputnum1.value);
-    var numvalue2 = Number(inputnum2.value);
-    var vychitanye = numvalue1 - numvalue2;
-    alert(vychitanye);
-}
-function onButtonMultClic() {
-    var inputnum1 = document.getElementById('inputnum1');
-    var inputnum2 = document.getElementById('inputnum2');
-    var numvalue1 = Number(inputnum1.value);
-    var numvalue2 = Number(inputnum2.value);
-    var multipl = numvalue1 * numvalue2;
-    alert(multipl);
-}
-function onButtonDiscClic() {
-    var inputnum1 = document.getElementById('inputnum1');
-    var inputnum2 = document.getElementById('inputnum2');
-    var numvalue1 = Number(inputnum1.value);
-    var numvalue2 = Number(inputnum2.value);
-    var discT = numvalue1 / numvalue2;
-    alert(discT);
+var inputnum1 = document.getElementById('inputnum1');
+var inputnum2 = document.getElementById('inputnum2');
+
+var mathoperations = document.getElementsByClassName('buttonOp');
+
+function makeOperation(operationCode) {
+    var Number1 = Number(inputnum1.value);
+    var Number2 = Number(inputnum2.value);
+    if (operationCode === '+') {
+        var result = Number1 + Number2;
+    } else if (operationCode === '-') {
+        var result = Number1 - Number2;
+    } else if (operationCode === '*') {
+        var result = Number1 * Number2;
+    } else if (operationCode === '/') {
+        var result = Number1 / Number2;
+    } else {
+        window.alert('Error');
+    }
+    window.alert(result);
 }
 
-buttonPlus.addEventListener('click', onButtonPlusClic);
-buttonMinus.addEventListener('click', onButtonMinusClic);
-buttonMult.addEventListener('click', onButtonMultClic);
-buttonDisc.addEventListener('click', onButtonDiscClic);
+function onOperationButtonClick(eventObject) {
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation); 
+}
+
+for (var i = 0; i < mathoperations.length; i++) {
+    var button= mathoperations[i];
+    button.addEventListener('click', onOperationButtonClick);    
+}
+
